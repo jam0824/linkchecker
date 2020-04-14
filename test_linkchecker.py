c53e63363d660d558a067b97d84a8902c5619373
m <aku_soku_zan_1bantai@hotmail.com>
2020-04-14 09:09:41 +0900

    テストコードを少し詳細に

--- test_linkchecker.py
+++ test_linkchecker.py
@@ -2,6 +2,8 @@ import unittest
 import classlinkchecker
 
 class Test_LinkChecker(unittest.TestCase):
+    
+
     def test_get_page_source_ページ存在時の確認(self):
         page_info = classlinkchecker.LinkChecker().get_page_info('http://testerchan.hatenadiary.com/')
         self.assertEqual(
@@ -11,12 +13,35 @@ class Test_LinkChecker(unittest.TestCase):
 
     def test_get_list_url_URLがとってこれた時の確認(self):
         lc = classlinkchecker.LinkChecker()
-        page_info = lc.get_page_info('http://testerchan.hatenadiary.com/')
-        list_url = lc.get_list_url(page_info.url, page_info.text)
+        html = '<html><head></head><body>'
+        html += '<a href="foo.html"></a>'
+        html += '<a href="bar.html"></a>'
+        html += '</body></html>'
+        list_url = lc.get_list_url('https://testerchan.hatenadiary.com', html)
         #print(list_url)
-        self.assertIn('https://manga.line.me/indies/product/detail?id=2358',list_url)
+        self.assertIn('https://testerchan.hatenadiary.com/foo.html',list_url)
 
-    def test_check_url_status(self):
+    def test_get_list_url_URLでNoneが含まれていないか確認(self):
+        lc = classlinkchecker.LinkChecker()
+        html = '<html><head></head><body>'
+        html += '<a name="test"></a>'
+        html += '</body></html>'
+        list_url = lc.get_list_url('https://testerchan.hatenadiary.com', html)
+        #print(list_url)
+        expected = []
+        self.assertEqual(expected,list_url)
+
+    def test_get_list_url_URLで絶対パスが絶対パスのままか確認(self):
+        lc = classlinkchecker.LinkChecker()
+        html = '<html><head></head><body>'
+        html += '<a href="https://testerchan.hatenadiary.com/test.html"></a>'
+        html += '</body></html>'
+        list_url = lc.get_list_url('https://testerchan.hatenadiary.com', html)
+        #print(list_url)
+        expected = 'https://testerchan.hatenadiary.com/test.html'
+        self.assertIn(expected,list_url)
+
+    def test_integration_check_url_status(self):
         lc = classlinkchecker.LinkChecker()
         page_info = lc.get_page_info('https://www.testerchan.work/')
         list_url = lc.get_list_url(page_info.url, page_info.text)
